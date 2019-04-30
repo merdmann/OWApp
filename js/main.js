1f9c7c44875b0455f6ba5bc6140b765be275f2f1
DESKTOP-6ST7TKK\michaelerdmann <michael.erdmann@snafu.de>
2019-04-30 07:54:21 +0200

    Initial start Day 2. Reads all data, Search implemtnented

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', function () {
     console.log("DOMContentLoaded")
+    const MS = 1000
+    const MIN = 60;
+    let  id;
+
+    /* search by name and return the id */
+    function Search( name) {
+    	let result;
+
+		Cities.forEach( function(city) {
+			result = city.name==name ? city.id : null;
+		});
+
+		return result;
+	}
 
     function main(data) {
         const pageTitle = document.title;
         console.log("main :" + pageTitle);
 
-        fetchData("http://api.openweathermap.org/data/2.5/weather/q=Berlin");
+        fetchData("http://api.openweathermap.org/data/2.5/weather?q=Berlin");
 
         // install the search-text handler
         const _search_text_ = document.getElementById("search-text");
         _search_text_.addEventListener("change", function () {
-        	Cities.forEach( function(city) {
-        		console.log(city); 
-        	})
-            console.log(_search_text_.value);
+            id = Search( _earch_text_.value);
         })
+    }
+        /* UPDATE every 10 mins.
+        window.setTimeout(function () {
+        	fetchData(fetchData("http://api.openweathermap.org/data/2.5/weather?q=Berlin");)
+        }, MS * NIN * 10 );
 
     } /* main */
 
@@ -29,8 +45,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // fetches data from the  server
     function fetchData(url) {
-        const key = "6f88b36c3ba927bbc4676605fc738ae3"
-        const myurl = url + "&appid="+key
+        const key = "6f88b36c3ba927bbc4676605fc738ae3";
+        const myurl = url + "&appid="+key;
         console.log(myurl); 
         console.log("fetching" + myurl);
         fetch(url + "&appid=" + key)
